be03ffce7836ba7a0ee118f98899e6712de8a9ab
Sahil Sagvekar <sahilsagvekar230@gmail.com>
2024-06-08 23:52:23 +0530

    Update axiosConfig.js

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: 'https://stack-overflow-clone-ogru.onrender.com' });
+const API = axios.create({ baseURL: 'https://stack-overflow-clone-4-p3i6.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("Profile")) {
